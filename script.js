f14a6df05b1fcce79413289bd923c252e126222e
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-06 03:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
-
+    const firstName = getElementById("first").value;
+	const lastName = getElementById("last").value;
+	alert(`${firstName} ${lastName}`);
 }
+
+document.getElementById("btn").onclick = getFormvalue;
